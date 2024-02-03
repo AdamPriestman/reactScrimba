7f26e2aff19dc1c3e06277d1bc10efc9f7ebafc8
AdamPriestman <adampriestman@gmail.com>
2024-02-03 21:45:39 +0000

    created my first component

--- firstReact/index.js
+++ firstReact/index.js
@@ -1,1 +1,15 @@
-ReactDOM.render(<h1>Hi, my name is Adam!</h1>, document.getElementById("root"))
+
+// React component
+function HeaderComponentExample() {
+  return (
+    <h2>I'm learning React!</h2>
+  )
+}
+
+ReactDOM.render(
+  <div>
+    <h1>Hi, my name is Adam!</h1>
+    <HeaderComponentExample />
+  </div>,
+  document.getElementById("root")
+)
