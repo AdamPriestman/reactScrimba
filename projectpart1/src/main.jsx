0d2db810a3f63564c3ebfe1d8197f14cd95dc2bd
AdamPriestman <adampriestman@gmail.com>
2024-02-06 19:25:10 +0000

    project part 1 completed

---
+++ projectpart1/src/main.jsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import reactLogo from './assets/react.svg'
+
+const page = (
+  <div>
+    <img src={reactLogo} alt="react logo" />
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well oover 100k stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, inclusing mobile apps</li>
+    </ul>
+  </div>
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    page
+)
