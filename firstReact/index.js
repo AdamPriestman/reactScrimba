d1fc827ab00151fae357157939f54b3d65e04da0
AdamPriestman <adampriestman@gmail.com>
2024-02-06 19:25:44 +0000

    cleanup from debugging

--- firstReact/index.js
+++ firstReact/index.js
@@ -1,15 +1,29 @@
 
 // React component
-function HeaderComponentExample() {
-  return (
-    <h2>I'm learning React!</h2>
-  )
-}
+// function HeaderComponentExample() {
+//   return (
+//     <h2>I'm learning React!</h2>
+//   )
+// }
 
-ReactDOM.render(
-  <div>
-    <h1>Hi, my name is Adam!</h1>
-    <HeaderComponentExample />
-  </div>,
-  document.getElementById("root")
+// ReactDOM.render(
+//   <div>
+//     <h1>Hi, my name is Adam!</h1>
+//     <HeaderComponentExample />
+//   </div>,
+//   document.getElementById("root")
+// )
+
+
+const navbar = (
+  <nav>
+    <h1>Adam's Website</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
 )
+
+ReactDOM.render(navbar, document.getElementById("root"))
