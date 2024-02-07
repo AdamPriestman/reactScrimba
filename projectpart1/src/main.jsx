e85b1a0b663d98f8bcbbecc1877c84f007ebf30c
AdamPriestman <adampriestman@gmail.com>
2024-02-07 17:18:50 +0000

    created react component page

--- projectpart1/src/main.jsx
+++ projectpart1/src/main.jsx
@@ -1,22 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import reactLogo from './assets/react.svg'
-
-const page = (
-  <div>
-    <img src={reactLogo} alt="react logo" />
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100k stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, inclusing mobile apps</li>
-    </ul>
-  </div>
-)
+import Page from './Page.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  page
+  <Page />
 )
