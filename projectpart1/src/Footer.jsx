88ec814901c972338f2b79076ff483df1e8af3f6
AdamPriestman <adampriestman@gmail.com>
2024-02-17 03:43:45 +0000

    removed old folders

--- projectpart1/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer () {
-  return (
-    <footer>
-        <small>© 2024 Priestman development. All rights reserved.</small>
-    </footer>
-  )
-}
-
-export default Footer
--- projectpart1/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Page from './Page.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Page />
-)
